d2b3a3d2e5a87a7f9fe058724beba92fed8da122
Tom Hartman <thomas.lees.hartman@gmail.com>
2021-02-06 13:00:11 -0500

    Basic App Setup
    Setup a basic app using the appfactory design pattern

---
+++ catalog/app/appfactory.py
@@ -0,0 +1,32 @@
+###############################################################################
+## appfactory.py for catalog                                                 ##
+## Copyright (c) 2020 Tom Hartman (thomas.lees.hartman@gmail.com)            ##
+##                                                                           ##
+## This program is free software; you can redistribute it and/or             ##
+## modify it under the terms of the GNU General Public License               ##
+## as published by the Free Software Foundation; either version 2            ##
+## of the License, or the License, or (at your option) any later             ##
+## version.                                                                  ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of            ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ##
+## GNU General Public License for more details.                              ##
+###############################################################################
+
+### Commentary ## {{{
+##
+## 
+##
+## }}}
+
+### appfactory ## {{{
+from flask import Flask
+from config.config import DevConfig
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_obj(config)
+
+    return app
+## }}}
