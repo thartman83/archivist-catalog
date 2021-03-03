4ae2a210227e64cfeba9c113a4d95cb81f363b1e
Tom Hartman <thomas.lees.hartman@gmail.com>
2021-03-03 18:36:15 -0500

    Record storage updates
    Updating record routes to use the new storage common functions
    
    Associated testing of record function updates.

--- app/routes/record.py
+++ app/routes/record.py
@@ -51,7 +51,7 @@ def createRecord():
             }, 500)
 
     # Add caluclated and storage values, then add to the record dictionary
-    json['location'] = path
+    json['location'] = str(path)
     json['hash'] = hash.hexdigest()
     json['size'] = filesize
 
@@ -190,7 +190,7 @@ def saveFileToStorage(data):
     cfg = current_app.config
     
     hash = hashlib.md5(decodedData)
-    path = storage.storeObject(StorageLocation.RECORD, decodedDate, hash)
+    path = storage.storeObject(storage.StorageLocations.RECORD, decodedData, str(hash))
 
     return hash, size(data), path
 
@@ -203,7 +203,7 @@ def validateRecordData(recordData):
     valid = True
     res = dict()
     invalidData = dict({ 'status': 'Invalid request', 'msg': [] })
-
+ 
     # check if there is any data at all
     if recordData is None:
         valid = False
