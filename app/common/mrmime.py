2b0e1226b34052172770895a5b0c0067b6091dd6
Tom Hartman <thomas.lees.hartman@gmail.com>
2021-03-09 20:45:34 -0500

    MrMime
    Added a common class for mimetype based functions.
    
    Currently it only supports pdf types and will convert a pdf to text
    and PIL images as pages.

---
+++ app/common/mrmime.py
@@ -0,0 +1,89 @@
+###############################################################################
+## mrmime.py for archivist catalog common modules                           ##
+## Copyright (c) 2021 Tom Hartman (thomas.lees.hartman@gmail.com)            ##
+##                                                                           ##
+## This program is free software; you can redistribute it and/or             ##
+## modify it under the terms of the GNU General Public License               ##
+## as published by the Free Software Foundation; either version 2            ##
+## of the License, or the License, or (at your option) any later             ##
+## version.                                                                  ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of            ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ##
+## GNU General Public License for more details.                              ##
+###############################################################################
+
+### Commentary ## {{{
+##
+## Mime type functions 
+##
+## }}}
+
+### mrmime ## {{{
+import PyPDF2
+from pathlib import Path
+from PIL import Image
+from pdf2image import convert_from_path
+import io
+
+class MimeBase(object):
+
+    def __init__(self, mimetype):
+        self.mimetype = mimetype
+
+class PDFMime(MimeBase):
+
+    def textify(self, filePath):
+        pdf = open(str(filePath), 'rb')
+
+        reader = PyPDF2.PdfFileReader(pdf)
+        text = ""
+
+        for page in reader.pages:
+            text += page.extractText()
+
+        return text
+
+    def paginate(self, filePath):
+        imgs = convert_from_path(filePath)
+
+        # pdf = open(str(filePath), 'rb')
+
+        # reader = PyPDF2.PdfFileReader(pdf)
+        # imgs = []
+        
+        # for page in reader.pages:
+        #     pdfOut = PyPDF2.PdfFileWriter()
+        #     pdfOut.addPage(page)
+            
+        #     pdfBytes = io.BytesIO()            
+        #     pdfOut.write(pdfBytes)
+        #     pdfBytes.seek(0)
+        #     img = Image.open(pdfBytes)
+        #     imgs.append(img)
+
+        return imgs
+
+def docMime(MimeBase):
+
+    def textify(self):
+        pass
+
+    def paginate(self):
+        pass
+
+def imageMime(MimeBase):
+
+    def textify(self):
+        pass
+
+    def paginate(self):
+        pass
+
+MrMime = {
+    "application/pdf": PDFMime("application/pdf")
+}
+
+
+## }}}
---
+++ tests/test_mrmime.py
@@ -0,0 +1,71 @@
+###############################################################################
+## test_mrmime.py for archivist catalog unit test module                     ##
+## Copyright (c) 2021 Tom Hartman (thomas.lees.hartman@gmail.com)            ##
+##                                                                           ##
+## This program is free software; you can redistribute it and/or             ##
+## modify it under the terms of the GNU General Public License               ##
+## as published by the Free Software Foundation; either version 2            ##
+## of the License, or the License, or (at your option) any later             ##
+## version.                                                                  ##
+##                                                                           ##
+## This program is distributed in the hope that it will be useful,           ##
+## but WITHOUT ANY WARRANTY; without even the implied warranty of            ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             ##
+## GNU General Public License for more details.                              ##
+###############################################################################
+
+### Commentary ## {{{
+##
+## Unit tests for mrmime
+##
+## }}}
+
+### test_mrmime ## {{{
+
+from app.common.mrmime import MrMime
+from PIL import Image, ImageChops
+
+def test_pdfTextify():
+    """
+    GIVEN a PDFMime object
+    WHEN the textify method is invoke
+    WHEN the parameter passed is a valid pdf file path
+    THEN the textify function will return the text of the pdf
+    """
+    
+    pdfMime = MrMime["application/pdf"]
+    pdfFile = "tests/data/SamplePDF.pdf"
+    textFile = "tests/data/SampleText.txt"
+    
+    textified = pdfMime.textify(pdfFile)
+    expectedText = ""
+
+    with open(textFile, 'r') as file:
+        expectedText = file.read()
+
+    assert textified == expectedText
+    
+
+def test_pdfPaginate():
+    """
+    GIVEN a PDFMime object
+    WHEN the textify method is invoke
+    WHEN the parameter passed is a valid pdf file path
+    THEN the textify function will return the text of the pdf
+    """
+
+    pdfMime = MrMime["application/pdf"]
+    pdfFile = "tests/data/SamplePDF.pdf"
+    imgFileBase = "tests/data/SampleImages-{}.tif"
+
+    pages = pdfMime.paginate(pdfFile)
+    assert len(pages) == 2
+
+    idx = 1
+    for p in pages:
+        imgPath = imgFileBase.format(idx)
+        diff = ImageChops.difference(p, Image.open(imgPath)) 
+        assert diff.getbbox()
+        idx = idx + 1
+
+## }}}
